fe311d5a511ace165b29a5d8a5622f226a9b6e5b
Kulujer <kulujer@126.com>
2017-03-31 14:06:26 +0800

    这是一个测试

---
+++ 测试/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 测试
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from Star";
+            OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=MyQQ.accdb;");
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand(sql ,conn);
+            OleDbDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            textBox1.Text = Convert.ToString(reader[1]);
+            textBox2.Text = Convert.ToString(reader[0]);
+            reader.Close();
+            conn.Close();
+        }
+    }
+}
